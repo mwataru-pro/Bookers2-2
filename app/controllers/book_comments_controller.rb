1902b9a4d044b21906d771126299c75c9df7f142
mwataru-pro <m1130wata@gmail.com>
2020-05-18 16:24:32 +0900

    [Update] 削除機能の訂正：指定したコメントを削除

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,6 @@
 class BookCommentsController < ApplicationController
+
+
 	def create
 		@book = Book.find(params[:book_id])
 		@book_comment = current_user.book_comments.new(book_comment_params)
@@ -12,9 +14,9 @@ class BookCommentsController < ApplicationController
 
 	def destroy
 		@book = Book.find(params[:book_id])
-		@book_comment = current_user.book_comments.find_by(params[:book_comment_id])
+		book_comment = @book.book_comments.find(params[:id])
 		# コメントを削除
-		@book_comment.destroy
+		book_comment.destroy
 		#同じページに戻る
 		redirect_back(fallback_location: root_path)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
   	resource :favorite, only: [:create, :destroy]
-  	resource :book_comments, only: [:create,:destroy]
+  	resources :book_comments, only: [:create,:destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update]
